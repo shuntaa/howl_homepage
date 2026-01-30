d6bc3785109782ffd7b75b100bf2aed6b89caa78
shunta <shuntadoi27@gmail.com>
2026-01-31 01:00:29 +0900

    fixed all errors

--- pages/1_Leaderboard.py
+++ pages/1_Leaderboard.py
@@ -4,7 +4,7 @@ import numpy as np
 import sys
 import os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pages._db import init_connection, load_data, assign_percentile_title
 
 st.header("🏆 Player Rating")
